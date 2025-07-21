238e2f671910564dfaa24bc33e64de1f866dda1f
Diego Caro <diegocaro@gmail.com>
2025-07-21 18:06:36 -0400

    fix settings

--- ahorratron/api/auth.py
+++ ahorratron/api/auth.py
@@ -3,7 +3,7 @@
 from fastapi import HTTPException, Security, status
 from fastapi.security.api_key import APIKeyHeader
 
-from ahorratron.api.config import settings
+from ahorratron.api.config import get_settings
 
 api_key_header = APIKeyHeader(name="X-API-KEY", auto_error=True)
 
@@ -21,6 +21,7 @@ async def verify_api_key(api_key: str = Security(api_key_header)) -> str:
     Raises:
         HTTPException: If the API key is invalid
     """
+    settings = get_settings()
     if api_key != settings.api_key:
         raise HTTPException(
             status_code=status.HTTP_401_UNAUTHORIZED,
