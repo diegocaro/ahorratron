7facea1795667f1ce40312548e64bd2bb84fd2d7
Diego Caro <diegocaro@gmail.com>
2025-08-10 17:41:25 -0400

    feat: Update JWT secret handling and improve logging in account retrieval

--- ahorratron/sync_api/main.py
+++ ahorratron/sync_api/main.py
@@ -20,7 +20,7 @@ SECRET_KEY = bytes.fromhex(os.environ["JWE_SECRET_KEY"])  # Must be 32 bytes for
 TOKEN_DURATION = datetime.timedelta(hours=12)
 JWE_ALGORITHM = "A256GCM"
 
-JWT_SECRET = "test_jwt_secret"  # os.environ["JWT_SECRET_KEY"]  # TODO: UPDATE THIGS and use a separate secret for JWT signing
+JWT_SECRET_KEY = os.environ["JWT_SECRET_KEY"]
 JWT_ALGORITHM = "HS256"  # Or RS256 if you want asymmetric signing
 
 app = FastAPI()
@@ -44,16 +44,17 @@ async def log_request_middleware(
 
 # Generate encrypted JWT (JWE) from a dictionary
 def create_encrypted_token(data: SessionData) -> str:
-    exp = int((datetime.datetime.now(datetime.UTC) + TOKEN_DURATION).timestamp())
     enc_payload = data.model_dump()
-    enc_payload["exp"] = exp
     enc_token = jwe.encrypt(
         json.dumps(enc_payload), SECRET_KEY, algorithm=JWE_ALGORITHM
     )
     enc_token_str = enc_token.decode() if isinstance(enc_token, bytes) else enc_token
 
-    payload = {"enc_token": enc_token_str, "exp:": exp}
-    token = jwt.encode(payload, JWT_SECRET, algorithm=JWT_ALGORITHM)
+    payload = {
+        "enc_token": enc_token_str,
+        "exp": int((datetime.datetime.now(datetime.UTC) + TOKEN_DURATION).timestamp()),
+    }
+    token = jwt.encode(payload, JWT_SECRET_KEY, algorithm=JWT_ALGORITHM)
     return token
 
 
@@ -61,7 +62,7 @@ def create_encrypted_token(data: SessionData) -> str:
 def get_decrypted_token(x_api_key: str = Header(..., alias="X-API-KEY")) -> SessionData:
     try:
         # Decode JWT
-        payload = jwt.decode(x_api_key, JWT_SECRET, algorithms=[JWT_ALGORITHM])
+        payload = jwt.decode(x_api_key, JWT_SECRET_KEY, algorithms=[JWT_ALGORITHM])
         exp = payload.get("exp")
         if exp and datetime.datetime.now(datetime.UTC).timestamp() > exp:
             raise HTTPException(status_code=401, detail="Token expired")
@@ -91,7 +92,7 @@ async def auth(request: UserData):
 
 @app.get("/accounts", response_model=AccountsResponse)
 def get_accounts(itemId: str, session_data: SessionData = Depends(get_decrypted_token)):
-    print(f"Session data: {session_data}")
+    logger.debug(f"Session data: {session_data}")
     connector = get_connector(session_data.user_data)
     response = connector.get_accounts(itemId=itemId)
     logger.debug(f"Accounts response: {response}")
