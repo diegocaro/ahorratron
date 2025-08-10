1473aec3cb0d60f7905c5c5b8f78786c5db3fe33
Diego Caro <diegocaro@gmail.com>
2025-08-10 17:27:22 -0400

    feat: Update environment variables in conftest.py for testing configuration

--- tests/conftest.py
+++ tests/conftest.py
@@ -2,10 +2,11 @@ import os
 
 os.environ["ENV_FILE"] = "tests/testing.env"
 
-
+# Update these per institution
 os.environ["BANK_URL"] = "https://example.com/bank"
 os.environ["BANK_USER"] = "test_user"
 os.environ["BANK_PASSWORD"] = "test_password"
+os.environ["BANK_LOGIN_URL"] = "https://example.com/login"
 os.environ["BANK_API_BASE_URL"] = "https://api.example.com/bank"
 
 os.environ["HEADER_REFERER"] = "https://example.com/referer"
