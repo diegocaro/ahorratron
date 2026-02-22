806187c1f197f59a483197aa48f8f64bf20fdac2
Diego Caro <diegocaro@gmail.com>
2026-02-22 13:03:28 -0300

    refactor: Remove unused connector ID parsing logic from UserData model

--- ahorratron/sync_api/models/core_models.py
+++ ahorratron/sync_api/models/core_models.py
@@ -6,17 +6,6 @@ class UserData(BaseModel, frozen=True):
     clientSecret: str
     connector_id: str = "banco_de_chile"
 
-    @model_validator(mode="before")
-    @classmethod
-    def parse_connector_id(cls, data):
-        if isinstance(data, dict) and "clientId" in data:
-            client_id = data["clientId"]
-            if ";" in client_id:
-                connector_id, real_client_id = client_id.split(";", 1)
-                data["connector_id"] = connector_id
-                data["clientId"] = real_client_id
-        return data
-
     def __str__(self) -> str:
         return f"UserData(clientId={self.clientId}, connector_id={self.connector_id})"
 
