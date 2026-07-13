5cd3a2226edb74c1b765ff2d2a2cb8442f3d48ad
Diego Caro <diegocaro@gmail.com>
2026-07-13 11:04:54 -0400

    refactor: remove to_utc helper from date parsing in models

--- ahorratron/sync_api/institutions/banco_de_chile/models.py
+++ ahorratron/sync_api/institutions/banco_de_chile/models.py
@@ -3,8 +3,6 @@ from enum import StrEnum
 
 from pydantic import BaseModel
 
-from ahorratron.sync_api.utils.helpers import to_utc
-
 DATE_FORMAT_MOVIMIENTO_CARTOLA = "%Y%m%d %H:%M:%S"
 DATE_FORMAT_HORA_CONSULTA = "%d/%m/%Y %H:%M"
 DATE_FORMAT_NO_FACTURADO = "%d/%m/%Y %H:%M:%S"
@@ -131,11 +129,9 @@ class MovimientoNoFacturado(BaseModel):
     @property
     def fecha_transaccion_iso(self) -> datetime:
         # example: 30/07/2025 16:44:29
-        return to_utc(
-            datetime.strptime(
-                f"{self.fechaTransaccionString} {self.horaAutorizacion}",
-                DATE_FORMAT_NO_FACTURADO,
-            )
+        return datetime.strptime(
+            f"{self.fechaTransaccionString} {self.horaAutorizacion}",
+            DATE_FORMAT_NO_FACTURADO,
         )
 
     @property
@@ -187,9 +183,8 @@ class Movimiento(BaseModel):
 
     @property
     def fecha_isoformat(self) -> datetime:
-        # example input: 20250730 16:44:29
-        # example output: 2025-07-30T20:44:29+00:00
-        return to_utc(datetime.strptime(self.fecha, DATE_FORMAT_MOVIMIENTO_CARTOLA))
+        # example: 20250730 16:44:29
+        return datetime.strptime(self.fecha, DATE_FORMAT_MOVIMIENTO_CARTOLA)
 
 
 class GetCartolaResponse(BaseModel):
@@ -208,11 +203,9 @@ class GetCartolaResponse(BaseModel):
 
     @property
     def hora_consulta_iso(self) -> datetime:
-        return to_utc(
-            datetime.strptime(
-                self.horaConsulta.replace(DATE_REPLACE_STRING, ""),
-                DATE_FORMAT_HORA_CONSULTA,
-            )
+        return datetime.strptime(
+            self.horaConsulta.replace(DATE_REPLACE_STRING, ""),
+            DATE_FORMAT_HORA_CONSULTA,
         )
 
 
